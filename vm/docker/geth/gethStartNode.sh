e2d6ee927a31b7bfe4f298271fe23aa972176583
Robert Zaremba <robert.zaremba@scale-it.pl>
2017-05-10 19:53:20 +0200

    fixing the image version and the the node discovery

--- vm/docker/geth/gethStartNode.sh
+++ vm/docker/geth/gethStartNode.sh
@@ -3,10 +3,9 @@ source env.sh
 export NETWORK_DIR='/network'
 export NODE_PORT=10000
 export NODE_IP='0.0.0.0'    # your local address you want to bind on
-export NODE_PORT_RPC=10001  # your local port for RPC
-export NETWORK_IDS=(2017040499 2017042099)
+export NODE_PORT_RPC=10001  # your local port for RPC, default=8545
+export NETWORK_IDS=(2017042099)
 export NETWORK_ID_=${NETWORK_IDS[0]}
-export ENODE='enode://56b1d55fa3a314a053f12258990f4477533b7b37602681578d1220c56db05505a7af23cec0004c85ecda4f096f1b2f19cccd54159d8623c072dbfb8bf6b30203@195.154.221.20:10000'
 
 if [ -z $network_id ]
 then
@@ -30,4 +29,6 @@ fi
 echo "Network_id: ${NETWORK_ID_}"
 
 echo "Starting Geth..."
-./geth --etherbase 0 --datadir=$NETWORK_DIR/$NODE_NAME --port=$NODE_PORT --rpc --rpcapi="admin,eth,miner,eth,web3,personal,txpool" --rpcaddr=$NODE_IP --rpcport=$NODE_PORT_RPC --ipcdisable --networkid=$NETWORK_ID_ --identity=$NODE_NAME --nodiscover --mine --minerthreads=2  --bootnodes $ENODE
+# we are using static-nodes.json (set up in gethInitNode.sh)
+#    --bootnodes $ENODE
+./geth --etherbase 0 --datadir=$NETWORK_DIR/$NODE_NAME --port=$NODE_PORT --rpc --rpcapi="admin,eth,miner,eth,web3,personal,txpool" --rpcaddr=$NODE_IP --rpcport=$NODE_PORT_RPC --ipcdisable --networkid=$NETWORK_ID_ --identity=$NODE_NAME --nodiscover --mine --minerthreads=2  --fast --mine
