e03b05e7765ad4904373d2313e69aa0441218ae9
Cyril Lapinte <cyril.lapinte@free.fr>
2017-06-01 13:43:56 +0200

    Fixing Windows/Vagrant issues, Improving .gitattributes

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -6,6 +6,9 @@ sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial ma
 sudo apt-get update
 sudo apt-get install -y docker-engine make
 
+# 534Mb extra option - recommended
+# sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
+
 DOCKER_RUNNING=`sudo systemctl status docker | grep 'Main PID'`
 if [ "x$DOCKER_RUNNING" != "x" ];
 then
@@ -16,12 +19,20 @@ else
   exit -1
 fi
 
-sudo usermod -aG docker $(whoami)
+sudo usermod -a -G docker ubuntu
 
+ENVIRONMENT_DIR="/home/ubuntu/share/environment/docker"
+if [ -d $ENVIRONMENT_DIR ];
+then
+  echo "Share directory is available in ~/share"
+else
+  echo "Error: no share directory available '$ENVIRONMENT_DIR'"
+  exit -1
+fi
 
 echo "Creating Geth image"
-cd ..
+cd $ENVIRONMENT_DIR
 make build-geth
 
 echo "Creating Truffle image"
-make build-truffle
+#make build-truffle
