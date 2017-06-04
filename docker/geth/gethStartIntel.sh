fa782a431d79131af54a92f97d19973dd3f2fcc9
Cyril Lapinte <cyril.lapinte@free.fr>
2017-06-04 22:18:42 +0200

    Creating gethStartIntel.sh

---
+++ docker/geth/gethStartIntel.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Copyright 2017 - Dechain User Group
+
+source /home/eth/env.sh
+
+export NODE_ENV="private"
+export RPC_HOST="localhost"
+export RPC_PORT="10001"
+export LISTENING_PORT="10000"
+export INSTANCE_NAME="${NODE_NAME}_node"
+export CONTACT_DETAILS=""
+export WS_SERVER="ws://factory.shinit.net:15000"
+export WS_SECRET="20170420devchain"
+export VERBOSITY=2
+
+node /home/eth/eth-net-intelligence-api/app.js
+
